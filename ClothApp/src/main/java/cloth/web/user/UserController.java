ab7267b51ee541b644b76141f9da84ceade516f8
zaragozamartin91 <zaragozamartin91@gmail.com>
2016-02-29 10:46:55 -0300

    Se hizo un manejo de "usuario no encontrado"

--- ClothApp/src/main/java/cloth/web/user/UserController.java
+++ ClothApp/src/main/java/cloth/web/user/UserController.java
@@ -7,14 +7,11 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import cloth.model.User;
-import cloth.service.user.UserNotFoundException;
 import cloth.service.user.UserService;
 import cloth.web.AbstractClothController;
 import cloth.web.ClothController;
@@ -26,9 +23,9 @@ public class UserController extends AbstractClothController {
 	@Autowired
 	@Qualifier("dataUserService")
 	UserService userService;
-	
-//	@Autowired
-//	DummyUsersCreator dummyUsersCreator;
+
+	// @Autowired
+	// DummyUsersCreator dummyUsersCreator;
 
 	@RequestMapping(path = "/login", method = RequestMethod.GET)
 	public String login(Model model) {
@@ -37,24 +34,38 @@ public class UserController extends AbstractClothController {
 	}
 
 	@RequestMapping(path = "/login", method = RequestMethod.POST)
-	public String login(@Valid UserLoginForm userLoginForm, Errors errors, RedirectAttributes model) {
+	public String login(@Valid UserLoginForm userLoginForm, Errors errors, Model model) {
 		if (errors.hasErrors()) {
 			return "login";
 		}
 
-		User currentUser = userService.getUserFromEmail(userLoginForm.getEmail());
+		User currentUser;
+		String userEmail = userLoginForm.getEmail();
+		try {
+			currentUser = userService.getUserFromEmail(userEmail);
+		} catch (Exception e) {
+			setDefaultUserLoginForm(model);
+			setLoginErrorMessage(model, userEmail);
+			return "login";
+		}
+
 		setCurrentUser(currentUser, model);
 
 		return "redirect:/cloth";
 	}
 
-	@ExceptionHandler(UserNotFoundException.class)
-	public String userNotFoundHandler() {
-		System.err.println("User not found!");
-		return "redirect:/cloth";
+	private void setLoginErrorMessage(Model model, String userEmail) {
+//		String errorMessage = "User " + userEmail + " not valid";
+		model.addAttribute("loginErrorUserEmail", userEmail);
 	}
 
+	// @ExceptionHandler(UserNotFoundException.class)
+	// public String userNotFoundHandler() {
+	// System.err.println("User not found!");
+	// return "redirect:/cloth";
+	// }
+
 	private void setDefaultUserLoginForm(Model model) {
-		model.addAttribute(new UserLoginForm());
+		model.addAttribute("userLoginForm", new UserLoginForm());
 	}
 }
